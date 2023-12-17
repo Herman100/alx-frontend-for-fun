19c98e550730e94590510cdc5eef1c782d03d22e
Herman100 <hermankwamebour30@gmail.com>
2023-12-17 23:08:17 +0000

    headings

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 This module contains a function that converts a Markdown file to HTML.
+It parses Markdown headings and generates corresponding HTML.
 """
 
 import sys
 import os
+import re
 
 
 def markdown2html():
@@ -12,7 +14,8 @@ def markdown2html():
     Convert a Markdown file to HTML.
     """
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         exit(1)
 
     markdown_file = sys.argv[1]
@@ -20,7 +23,16 @@ def markdown2html():
         print(f"Missing {markdown_file}", file=sys.stderr)
         exit(1)
 
-    # Your code to convert markdown to html goes here
+    with open(markdown_file, 'r') as f:
+        lines = f.readlines()
+
+    html_lines = []
+    for line in lines:
+        match = re.match(r'(#{1,6})\s(.*)', line)
+        if match:
+            level = len(match.group(1))
+            content = match.group(2)
+            html_lines.append(f'<h{level}>{content}</h{level}>')
 
 
 if __name__ == "__main__":
