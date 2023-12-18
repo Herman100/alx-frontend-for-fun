9d0f92499cdaad9f0e16e7ca38fcc5994e83ded5
KB_100 <55487841+Herman100@users.noreply.github.com>
2023-12-18 05:26:22 +0000

    test code full markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,84 +1,96 @@
 #!/usr/bin/python3
-"""
-This module contains a function that converts a Markdown file to HTML.
-It parses Markdown headings and generates corresponding HTML.
-"""
 
+"""
+Markdown script using python.
+"""
 import sys
-import os
+import os.path
 import re
+import hashlib
 
-
-def markdown2html():
-    """
-    Convert a Markdown file to HTML.
-    """
+if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
+        print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
         exit(1)
 
-    markdown_file = sys.argv[1]
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
 
-    output_file = sys.argv[2]
+    with open(sys.argv[1]) as read:
+        with open(sys.argv[2], 'w') as html:
+            unordered_start, ordered_start, paragraph = False, False, False
+            # bold syntax
+            for line in read:
+                line = line.replace('**', '<b>', 1)
+                line = line.replace('**', '</b>', 1)
+                line = line.replace('__', '<em>', 1)
+                line = line.replace('__', '</em>', 1)
 
-    with open(markdown_file, 'r') as md_file:
-        markdown_content = md_file.read()
+                # md5
+                md5 = re.findall(r'\[\[.+?\]\]', line)
+                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
+                if md5:
+                    line = line.replace(md5[0], hashlib.md5(
+                        md5_inside[0].encode()).hexdigest())
 
-    html_content = convert_headings(markdown_content)
-    html_content = convert_lists(html_content)
-    html_content = convert_ordered_lists(html_content)
+                # remove the letter C
+                remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
+                remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
+                if remove_letter_c:
+                    remove_c_more = ''.join(
+                        c for c in remove_c_more[0] if c not in 'Cc')
+                    line = line.replace(remove_letter_c[0], remove_c_more)
 
-    with open(output_file, 'w') as html_file:
-        html_file.write(html_content)
+                length = len(line)
+                headings = line.lstrip('#')
+                heading_num = length - len(headings)
+                unordered = line.lstrip('-')
+                unordered_num = length - len(unordered)
+                ordered = line.lstrip('*')
+                ordered_num = length - len(ordered)
+                # headings, lists
+                if 1 <= heading_num <= 6:
+                    line = '<h{}>'.format(
+                        heading_num) + headings.strip() + '</h{}>\n'.format(
+                        heading_num)
 
+                if unordered_num:
+                    if not unordered_start:
+                        html.write('<ul>\n')
+                        unordered_start = True
+                    line = '<li>' + unordered.strip() + '</li>\n'
+                if unordered_start and not unordered_num:
+                    html.write('</ul>\n')
+                    unordered_start = False
 
-def convert_headings(markdown_content):
-    """
-    Convert Markdown headings to HTML.
-    """
-    headings = [
-        (r'###### (.*)', r'<h6>\1</h6>'),
-        (r'##### (.*)', r'<h5>\1</h5>'),
-        (r'#### (.*)', r'<h4>\1</h4>'),
-        (r'### (.*)', r'<h3>\1</h3>'),
-        (r'## (.*)', r'<h2>\1</h2>'),
-        (r'# (.*)', r'<h1>\1</h1>')
-    ]
+                if ordered_num:
+                    if not ordered_start:
+                        html.write('<ol>\n')
+                        ordered_start = True
+                    line = '<li>' + ordered.strip() + '</li>\n'
+                if ordered_start and not ordered_num:
+                    html.write('</ol>\n')
+                    ordered_start = False
 
-    for markdown_pattern, html_replace in headings:
-        markdown_content = re.sub(markdown_pattern,
-                                  html_replace, markdown_content)
+                if not (heading_num or unordered_start or ordered_start):
+                    if not paragraph and length > 1:
+                        html.write('<p>\n')
+                        paragraph = True
+                    elif length > 1:
+                        html.write('<br/>\n')
+                    elif paragraph:
+                        html.write('</p>\n')
+                        paragraph = False
 
-    return markdown_content
+                if length > 1:
+                    html.write(line)
 
-
-def convert_lists(markdown_content):
-    """
-    Convert Markdown unordered lists to HTML.
-    """
-    html_content = re.sub(r'^(?!\s*$)(?:- (.+)$\n?)+', r'<ul>\n\g<0></ul>',
-                          markdown_content, flags=re.MULTILINE)
-    html_content = re.sub(r'^- (.+)$', r'<li>\1</li>',
-                          html_content, flags=re.MULTILINE)
-
-    return html_content
-
-
-def convert_ordered_lists(markdown_content):
-    """
-    Convert Markdown ordered lists to HTML.
-    """
-    html_content = re.sub(r'^(?!\s*$)(?:\* (.+)$\n?)+', r'<ol>\n\g<0></ol>',
-                          markdown_content, flags=re.MULTILINE)
-    html_content = re.sub(r'^\* (.+)$', r'<li>\1</li>',
-                          html_content, flags=re.MULTILINE)
-
-    return html_content
-
-
-if __name__ == "__main__":
-    markdown2html()
+            if unordered_start:
+                html.write('</ul>\n')
+            if ordered_start:
+                html.write('</ol>\n')
+            if paragraph:
+                html.write('</p>\n')
+    exit (0)
